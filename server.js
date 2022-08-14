2ce7f9dfe9e1474937549d512668c5e5e33b9702
ryoichi-k <kazyuapple99@gmail.com>
2022-08-14 16:50:22 +0900

    add dotenv

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const port = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//mongodb接続
+mongoose.connect(
+    process.env.MONGOURL
+).then(() => {
+    console.log("db connect");
+}).catch((err) => {
+    console.log(err);
+});
 
 //ミドルウェア
 app.use("/api/users", userRoute);
