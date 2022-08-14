9c59b25367a5f267efd670fbf9bfad70dcfce52c
ryoichi-k <kazyuapple99@gmail.com>
2022-08-14 15:04:07 +0900

    add routes

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 const port = 3000;
 
 //ミドルウェア
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.get("/", (req, res) => {
     res.send("aiueo")
