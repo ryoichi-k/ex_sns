21649d835009cd52db27309e78c4f7c3b88c8096
ryoichi-k <kazyuapple99@gmail.com>
2022-08-15 15:56:15 +0900

    フォロー機能追加

--- routes/users.js
+++ routes/users.js
@@ -34,13 +34,50 @@ router.delete("/:id", async (req, res) => {
     }
 });
 //ユーザー情報取得
+router.get("/:id", async (req, res) => {
+    //user id の照合
 
+    try {
+        const user = await User.findById(req.params.id);
+        //passwordとupdatedAt情報を抜いた情報であるotherを返すように分割代入
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 
+});
 
-
-
-// router.get("/", (req, res) => {
-//     res.send("user router");
-// });
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            //user→フォローする対象ユーザー
+            const user = await User.findById(req.params.id);
+            //currentUser→自身
+            const currentUser = await User.findById(req.body.userId);
+            //既にフォローしているか
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({
+                    $push: {
+                        followers: req.body.userId,
+                    },
+                });
+                await currentUser.updateOne({
+                    $push: {
+                        followings: req.params.id,
+                    },
+                });
+                return res.status(200).json("follow success");
+            } else {
+                return res.status(403).json("あなたは既にこのユーザーをフォローしています。");
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(500).json("cannnot follow yourself");
+    }
+})
 
 module.exports = router;
