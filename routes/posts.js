e6200eae4374b75d80d91466092e33277a0327c8
ryoichi-k <kazyuapple99@gmail.com>
2022-08-15 16:53:34 +0900

    投稿編集実装

--- routes/posts.js
+++ routes/posts.js
@@ -13,5 +13,21 @@ router.post("/", async (req, res) => {
         return res.status(500).json(err);
     }
 });
-
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+    try {
+        //postには更新したい投稿が入る
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({
+                $set: req.body,
+            });
+            return res.status(200).json("投稿編集に成功！");
+        } else {
+            return res.status(403).json("他の人の投稿を編集できません。");
+        }
+    } catch (err) {
+        return res.status(403).json(err);
+    }
+});
 module.exports = router;
