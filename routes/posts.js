fd7ae9b050e70758b93f6d47112aef76ddbc5fe4
ryoichi-k <kazyuapple99@gmail.com>
2022-08-15 16:39:59 +0900

    投稿新規作成

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    res.send("posts router");
+//投稿を作成する
+router.post("/", async (req, res) => {
+    //インスタンス化
+    const newPost = new Post(req.body);
+    try {
+        //DBに保存
+        const savedPost = await newPost.save();
+        return res.status(200).json(savedPost);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
 });
 
 module.exports = router;
