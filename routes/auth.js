42ba6ca3654f3410977244a95127bd61548d0697
ryoichi-k <kazyuapple99@gmail.com>
2022-08-15 13:37:24 +0900

    ログイン機能追加

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
             email: req.body.email,
             password: req.body.password,
         });
-        
+
         const user = await newUser.save();
         return res.status(200).json(user);
     } catch (err) {
@@ -17,8 +17,21 @@ router.post("/register", async (req, res) => {
     }
 });
 
-// router.get("/", (req, res) => {
-//     res.send("auth router");
-// });
+//ログイン
+router.post("/login", async (req, res) => {
+    try {
+        //emailでユーザーを一人抽出
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) return res.status(404).send("user not found");
+
+        const vailedPassword = req.body.password === user.password;
+        if (!vailedPassword) return res.status(400).json("invalid password");
+
+        return res.status(200).json(user);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
+//crud操作
+//ユーザー情報更新
+router.put("/:id", async (req, res) => {
+    //user id の照合
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                //全てのパラメータを指定
+                $set: req.body,
+            });
+            res.status(200).json("update success");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("sorry you cannnot change others information");
+    }
+});
+//ユーザー情報削除
+//ユーザー情報取得
 
 
 
