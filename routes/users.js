82b10915e4132088859761f25fc93934a82c1a3e
ryoichi-k <kazyuapple99@gmail.com>
2022-08-17 15:49:30 +0900

    return修正

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ router.get("/:id", async (req, res) => {
         const user = await User.findById(req.params.id);
         //passwordとupdatedAt情報を抜いた情報であるotherを返すように分割代入
         const { password, updatedAt, ...other } = user._doc;
-        res.status(200).json(other);
+        return res.status(200).json(other);
     } catch (err) {
         return res.status(500).json(err);
     }
