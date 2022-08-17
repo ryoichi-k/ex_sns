f2de2ee267ab46c43792949e7a5dca9887c41a6c
ryoichi-k <kazyuapple99@gmail.com>
2022-08-17 12:59:07 +0900

    timeline修正

--- routes/posts.js
+++ routes/posts.js
@@ -89,9 +89,9 @@ router.put("/:id/like", async (req, res) => {
 });
 
 //タイムラインの投稿取得
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
     try {
-        const currentUser = await User.findById(req.body.userId);
+        const currentUser = await User.findById(req.params.userId);
         //_idはcurrentUserのuseridのこと
         const userPosts = await Post.find({ userId: currentUser._id });
         //自分がフォローしている友達の投稿内容を全て取得する
