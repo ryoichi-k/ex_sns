3131707133b2161933c07597ab99bc6617a03fa5
ryoichi-k <kazyuapple99@gmail.com>
2022-08-20 10:56:24 +0900

    画像アップロード

---
+++ routes/upload.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+const multer = require("multer");
+
+
+const storage = multer.diskStorage({
+    //保存先
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      // cb(null, file.filename + "-" + Date.now());
+      cb(null, file.originalname);
+    //   cb(null, req.body.name);
+    },
+  });
+
+const upload = multer({storage});
+
+//画像アップロード
+router.post("/", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("画像のアップロードに成功しました");
+    } catch (err) {
+      console.log(err);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const uploadRoute = require("./routes/upload");
 const port = 3001;
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 //mongodb接続
@@ -18,10 +20,12 @@ mongoose.connect(
 });
 
 //ミドルウェア
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/upload", uploadRoute);
 
 app.get("/", (req, res) => {
     res.send("aiueo")
